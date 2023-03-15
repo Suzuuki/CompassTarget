f57fe852d29f38955ebedb235543ccfe3b340061
Sasuke <ultraswinya@gmail.com>
2023-03-15 16:58:46 +0300

    added coordinate setting for each world

--- src/main/java/ru/sasuke/compasstarget/CompassTarget.java
+++ src/main/java/ru/sasuke/compasstarget/CompassTarget.java
@@ -1,16 +1,15 @@
 package ru.sasuke.compasstarget;
 
-import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.Location;
-import org.bukkit.Material;
+import org.bukkit.*;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
 
+import javax.security.auth.login.Configuration;
 import java.util.ArrayList;
 import java.util.Objects;
 
@@ -42,21 +41,26 @@ public final class CompassTarget extends JavaPlugin implements CommandExecutor {
             return true;
         }
 
-        for(String wName : getConfig().getStringList("blocked-worlds")) {
-            if(Bukkit.getWorld(wName).equals(p.getWorld())) {
-                p.sendMessage(ChatColor.translateAlternateColorCodes('&', Objects.requireNonNull(getConfig().getString("block-world"))));
-                return true;
-            }
-        }
-
 
         try {
             int x = Integer.parseInt(args[0]);
             int z = Integer.parseInt(args[1]);
-            if(x > getConfig().getInt("x1") || x < getConfig().getInt("x2") || z > getConfig().getInt("z1") || z < getConfig().getInt("z2")) {
-                p.sendMessage(ChatColor.translateAlternateColorCodes('&', Objects.requireNonNull(getConfig().getString("set-error"))));
-                return true;
+
+
+            for(String key : getConfig().getConfigurationSection("worlds").getKeys(false)) {
+                int x1 = getConfig().getInt("worlds." + key + ".x1");
+                int x2 = getConfig().getInt("worlds." + key + ".x2");
+                int z1 = getConfig().getInt("worlds." + key + ".z1");
+                int z2 = getConfig().getInt("worlds." + key + ".z2");
+                if(Bukkit.getWorld(key).equals(p.getWorld())) {
+                    if(x > x1 || x < x2 || z > z1 || z < z2) {
+                        p.sendMessage(ChatColor.translateAlternateColorCodes('&', Objects.requireNonNull(getConfig().getString("set-error"))));
+                        return true;
+                    }
+                }
             }
+
+
 
             p.setCompassTarget(new Location(p.getWorld(), x, 0, z));
             p.sendMessage(ChatColor.translateAlternateColorCodes('&', Objects.requireNonNull(Objects.requireNonNull(getConfig().getString("set-success")).replace("%x", args[0]).replace("%z", args[1]))));
